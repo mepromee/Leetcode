29f9789435b1ae65a33a6a800158c187e8b69103
mepromee <58119316+mepromee@users.noreply.github.com>
2022-12-07 20:36:41 -0800

    Time: 290 ms (74.46%), Space: 14.5 MB (56.12%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -13,19 +13,23 @@ public:
         return solve(0,stones.size()-1,0,prefixSum);
     }
 private:
-    int dp[1001][1001][2];
+    int dp[1001][1001];
     int solve(int l, int r, int curTurn, vector<int>&prefixSum) {
         if(l > r) return 0;
-        if(dp[l][r][curTurn] != -1) return dp[l][r][curTurn];
+        if(dp[l][r] != -1) return dp[l][r];
 
         int prefixSum1 = getSubArrSum(l,r-1,prefixSum);
         int prefixSum2 = getSubArrSum(l+1,r,prefixSum);
         int r1 = solve(l,r-1,1-curTurn,prefixSum);
         int r2 = solve(l+1,r,1-curTurn,prefixSum);
-        dp[l][r][0] = max(prefixSum1+r1,prefixSum2+r2); // Alice
-        dp[l][r][1] = min(-prefixSum1+r1,-prefixSum2+r2); // Bob
+        if(curTurn) {
+            dp[l][r] = min(-prefixSum1+r1,-prefixSum2+r2); // Bob
+        }
+        else {
+            dp[l][r] = max(prefixSum1+r1,prefixSum2+r2); // Alice       
+        }
         
-        return dp[l][r][curTurn];
+        return dp[l][r];
     }
 
     int getSubArrSum(int l, int r, vector<int>&prefixSum) {
