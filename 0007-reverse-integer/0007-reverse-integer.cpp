c9e183b3313429c99b0f1baa88c11b284b01398a
mepromee <58119316+mepromee@users.noreply.github.com>
2022-12-10 19:54:42 -0800

    Time: 3 ms (69.29%), Space: 6 MB (6.87%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long int n = x;
-        long long int res = 0;
-        if(n < 0) n = -n;
+        int n = x, r = 0, d;
         while(n) {
-            res *= 10;
-            res += n%10;
-            if(!isIn32Bit(res)) {
-                res = 0;
+            d = n%10;
+            if(d < 0) d = -d;
+            if(isIn32Bit(r, d, x>=0)) {
+                r = r * 10 + d;
+            }
+            else {
+                r = 0;
                 break;
             }
             n/=10;
         }
-        
-        if(x < 0) return res * -1;
-        return res;
+        if(x < 0) r *= -1;
+        return r;
     }
 private:
-    bool isIn32Bit(long long int x) {
-        return x >= INT_MIN && x <= INT_MAX;
+    bool isIn32Bit(int cur, int d, bool isPos) {
+        if(isPos || d == 0) return cur <= (INT_MAX - d) / 10;
+        return cur <=  (INT_MAX - d + 1) / 10;
     }
 };
 /*
 amazon
 apple
 adobe
+
+r * 10 + d > int_max
+r * 10 > im - d
+r > im-d/10
+
+r*10 + d = im + 1
+r*10 = im-d+1
+
+-123
+
+r * 10 + d ) * -1 < intmin
+r * 10 + d > -intmin
+r * 10 > intmax+1 - d
+r > intmax+1 - d / 10
 */
