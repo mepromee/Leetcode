f398dfaa51e3b6b911dacd06afcab78e8c6f71fe
mepromee <58119316+mepromee@users.noreply.github.com>
2022-12-12 19:24:04 -0800

    Time: 15 ms (92.72%), Space: 9.9 MB (69.80%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -3,24 +3,27 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int ans = INT_MAX;
         int n = matrix.size();
-        vector<vector<int>> minSum = matrix;
+        vector<int> minSum = matrix[n-1];
         calc(matrix,minSum);
         for(int i = 0; i < n; i++) {
-            ans = min(ans, minSum[0][i]);
+            ans = min(ans, minSum[i]);
         }
         return ans;
     }
 private:
-    void calc(vector<vector<int>>&matrix, vector<vector<int>>&minSum) {
+    void calc(vector<vector<int>>&matrix, vector<int>&minSum) {
         int n = matrix.size();
+        vector<int>tmp;
         for(int i = n-2; i >= 0; i--) {
+            tmp = matrix[i];
             for(int j = 0;j<n; j++) {
-                int x = isValid(i+1,j-1,n) ? minSum[i+1][j-1] : INT_MAX;
-                int y = isValid(i+1,j,n) ? minSum[i+1][j] : INT_MAX;
-                int z = isValid(i+1,j+1,n)? minSum[i+1][j+1] : INT_MAX;
+                int x = isValid(i+1,j-1,n) ? minSum[j-1] : INT_MAX;
+                int y = isValid(i+1,j,n) ? minSum[j] : INT_MAX;
+                int z = isValid(i+1,j+1,n)? minSum[j+1] : INT_MAX;
                 
-                minSum[i][j]+= min(x,min(y,z));
+                tmp[j]+= min(x,min(y,z));
             }
+            minSum = tmp;
         }
     }
     bool isValid(int i, int j, int n) {
