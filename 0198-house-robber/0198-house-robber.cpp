84f47fd8bebf3ee85446a1fb0bdda3ae56b94b89
mepromee <58119316+mepromee@users.noreply.github.com>
2022-12-13 19:25:52 -0800

    Time: 0 ms (100.00%), Space: 7.7 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,18 +3,14 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        int includingPre = nums[1], notIncludingPre = nums[0];
-        int includingCur, notIncludingCur;
-        int ans = max(includingPre, notIncludingPre);
+        int rob1 = 0, rob2 = nums[0], cur;
+        int ans = max(rob1, rob2);
         
-        for(int i =2; i<n; i++) {
-            notIncludingCur = max(notIncludingPre, includingPre);
-            includingCur = nums[i] + notIncludingPre;
-            
-            ans = max(ans, max(notIncludingCur, includingCur));
-            
-            notIncludingPre = notIncludingCur;
-            includingPre = includingCur;
+        for(int i =1; i<n; i++) {
+            cur = max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = cur;
+            ans = max(ans, cur);
         }
         return ans;
     }
