8ad97114e697c6ac9d49ce0c08716580375b2a3d
mepromee <58119316+mepromee@users.noreply.github.com>
2022-12-14 18:57:17 -0800

    Time: 9 ms (85.10%), Space: 9.1 MB (49.79%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -2,49 +2,34 @@ class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<vector<pair<int,int>>>adj(n+1);
-        vector<int>vis(n+1);
-        int i = 0;
+        par = vector<int>(n+1);
+        for(int i = 0; i < n; i++) par[i] = i;
         for(auto edge: edges) {
-            //construct adjacency list
-            adj[edge[0]].push_back({edge[1],i});
-            adj[edge[1]].push_back({edge[0],i});
-            i++;
+            if(findPar(edge[0]) == findPar(edge[1])) {
+                return edge;
+            }
+            uni(edge[0], edge[1]);
         }
         
-        dfs(edges[0][0], edges[0][0], adj, vis);
-        
-        return edges[redundantEdgeId];
+        return {};
     }
 private:
-    int redundantEdgeId = -1;
-    int cycleStart = -1;
-    bool isCycle = false;
-    bool finished = false;
-    
-    void dfs(int cur, int par, vector<vector<pair<int,int>>>adj, vector<int>&vis) {
-        // mark to be in active path
-        vis[cur] = 1;
-        for(auto nxt: adj[cur]) {
-            if(nxt.first == par) continue;
-            // nxt one is in active path then it's in a cycle
-            if(vis[nxt.first]) {
-                redundantEdgeId = max(redundantEdgeId, nxt.second);
-                cycleStart = nxt.first;
-                isCycle = true;
-                return;
-            }
-            dfs(nxt.first, cur, adj, vis);
-            if(finished) return;
-            if(isCycle) {
-                redundantEdgeId = max(redundantEdgeId, nxt.second);
-                if(cur == cycleStart) finished = true;
-                return;
-            }
-        }
+    vector<int>par;
+    int findPar(int u) {
+        if(u == par[u]) return u;
+        return par[u] = findPar(par[u]);
+    }
+    void uni(int u, int v) {
+        int par_u = findPar(u);
+        int par_v = findPar(v);
+        
+        if(par_u < par_v) par[par_u] = par_v;   
+        else if(par_v < par_u) par[par_v] = par_u;
     }
 };
 /*
 amazon
 fb
+
+6:46
 */
