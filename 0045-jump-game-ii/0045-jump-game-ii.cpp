72eecf833fc3a1734113fb4fd99e6a7363feb202
mepromee <58119316+mepromee@users.noreply.github.com>
2023-02-08 22:44:17 -0800

    Time: 20 ms (66.05%), Space: 16.6 MB (84.20%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        set<int> st;
-        int n = nums.size(), mn, j;
-        vector<int>jumps(n);
-        jumps[n-1] = 0;
-        
-        for(int i = n-2; i>=0; i--) {
-            mn = i+nums[i] >= n-1 ? 1 : INT_MAX;
-            while(!st.empty()) {
-                set<int>::iterator itr = --st.end();
-                j = *itr * (-1);
-                if(j > i+nums[i] || min(n-1,j+nums[j]) > min(n-1,i+nums[i])) break;
-                cout<<i<<" removed "<<j<<endl;
-                st.erase(itr);
-            }
-            if(!st.empty()) {
-                auto itr = st.lower_bound(-(i+nums[i]));
-                if(itr != st.end()) {
-                    j = *itr * (-1);
-                    cout<<i<<" ("<<i+nums[i]<<") "<<j<<" "<<jumps[j]<<endl;
-                    if(jumps[j] != INT_MAX)
-                        mn = min(mn, jumps[j]+1);
-                }
-            }
-            st.insert(-i);
-            jumps[i] = mn;
-        }
-        
-        return jumps[0];
+    int jump(vector<int>& arr) {
+        if(arr.size()==1) return 0;
+	    int reach = arr[0], mx=0;
+	    int jump = 1;
+	    for(int i = 0; i<arr.size();i++) {
+		    if(reach >= arr.size()-1) break;
+		    if(i<=reach){
+			    mx = max(mx,i+arr[i]);
+		    }
+		    if(i==reach) {
+                reach = mx;
+                jump++;
+		    }
+	    }
+        return jump;
     }
 };
-/*
-9:09
-
--12 -10 -8 -6
-*/
