39db0b70f82fed8a04c8449366c6f230913da9f6
mepromee <58119316+mepromee@users.noreply.github.com>
2023-03-01 21:38:12 -0800

    Time: 34 ms (79.13%), Space: 71.5 MB (13.91%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,46 +11,24 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* cur1 = l1, *cur2 = l2, *ansHead = nullptr, *curNode = nullptr;
-        bool isHead = true;
-        int sum = 0, carry = 0, d1, d2;
+        ListNode *dummyHead = new ListNode(0);
+        ListNode* cur = dummyHead;
+        int d1,d2,sum,carry=0;
         
-        while(cur1 != nullptr || cur2 != nullptr) {
-            if(cur1 == nullptr) d1 = 0;
-            else {
-                d1 = cur1->val;
-                cur1 = cur1->next;
-            }
-            
-            if(cur2 == nullptr) d2 = 0;
-            else {
-                d2 = cur2->val;
-                cur2 = cur2->next;
-            }
-            
+        while(l1 != nullptr || l2 != nullptr || carry != 0) {
+            d1 = l1 == nullptr ? 0 : l1->val;
+            d2 = l2 == nullptr ? 0 : l2->val;
             sum = d1 + d2 + carry;
-            ListNode *newNode = new ListNode(sum%10);
-            if(isHead) {
-                curNode = newNode;
-                isHead = false;
-                ansHead = curNode;
-            }
-            else {
-                curNode->next = newNode;
-                curNode = newNode;
-            }
-            
             carry = sum/10;
+            ListNode *newNode = new ListNode(sum%10);
+            cur->next = newNode;
+            cur = newNode;
+            
+            l1 = l1 == nullptr ? l1 : l1->next;
+            l2 = l2 == nullptr ? l2 : l2->next;
         }
         
-        while(carry != 0) {
-            ListNode *newNode = new ListNode(carry%10);
-            curNode->next = newNode;
-            curNode = newNode;
-            carry = carry/10;
-        }
-        
-        return ansHead;
+        return dummyHead->next;
     }
 };
 /*
