a24d179eff928c73f4461feabdbb1c2e5330b5ba
mepromee <58119316+mepromee@users.noreply.github.com>
2023-03-01 21:15:38 -0800

    Time: 8 ms (94.78%), Space: 10.8 MB (44.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,vector<int>> hashmap;
+        unordered_map<int,int> hashmap;
         vector<int>ans;
         int rem;
         
         for(int i = 0; i < nums.size(); i++) {
-            if(hashmap.find(nums[i]) == hashmap.end()) {
-                hashmap[nums[i]] = vector<int>();
-            }
-            hashmap[nums[i]].push_back(i);
-        }
-        
-        for(int i = 0; i < nums.size(); i++) {
             rem = target - nums[i];
-            if(rem == nums[i]) {
-                if(hashmap.find(rem) != hashmap.end() && hashmap[rem].size() > 1) {
-                    ans = vector<int> {hashmap[rem][0], hashmap[rem][1]};
-                    break;
-                }
-            } 
-            else if(hashmap.find(rem) != hashmap.end()) {
-                ans = vector<int> {i, hashmap[rem][0]};
+            if(hashmap.find(rem) != hashmap.end()) {
+                ans = vector<int> {i, hashmap[rem]};
                 break;
             }
+            
+            hashmap[nums[i]] = i;
         }
         
         return ans;
