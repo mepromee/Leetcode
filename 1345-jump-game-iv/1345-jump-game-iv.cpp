d769424930f755b26a5681c2aed23ffce3909e9b
mepromee <58119316+mepromee@users.noreply.github.com>
2023-03-04 21:43:23 -0800

    Time: 347 ms (29.72%), Space: 102.7 MB (23.31%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
+        if(arr.size() == 1) return 0;
+        
         unordered_map<int,vector<int>> hashmap;
         unordered_map<int,bool>visited;
         queue<int>q;
@@ -13,10 +15,10 @@ public:
         q.push(0);
         visited[0] = true;
         while(!q.empty()) {
+            jumps++;
             for(int i = q.size(); i > 0; i--) {
                 int cur = q.front();
                 q.pop();
-                if(cur == arr.size()-1) return jumps;
                 vector<int> &nxtnodes = hashmap[arr[cur]];
                 nxtnodes.push_back(cur-1);
                 nxtnodes.push_back(cur+1);
@@ -25,12 +27,12 @@ public:
                     if(nxt >= 0 && nxt < arr.size() && !visited[nxt]) {
                         visited[nxt] = true;
                         q.push(nxt);
+                        if(nxt == arr.size()-1) return jumps;
                     }
                 }
                 
                 nxtnodes.clear();
             }
-            jumps++;
         }
         return jumps;
     }
