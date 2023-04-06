2fdef5f9221a877c51f1306a4a15912b492b819c
Sabera Mahmud Promee <58119316+mepromee@users.noreply.github.com>
2023-04-06 15:15:39 -0700

    Time: 28 ms (48.81%), Space: 13.5 MB (47.92%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,27 +11,27 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int minValId = -1, curMin = INT_MAX;
+        priority_queue<pair<int,ListNode*>>pq;
         ListNode *head, *cur;
         head = cur = new ListNode(-1);
         
-        while(true){
-            minValId = -1;
-            curMin = INT_MAX;
-            for(int i = 0; i < lists.size(); i++) {
-                if(lists[i] != nullptr) {
-                    if(curMin > lists[i]->val) {
-                        curMin = lists[i]->val;
-                        minValId = i;
-                    }
-                }
+        for(auto list: lists) {
+            if(list != nullptr) 
+                pq.push({-list->val, list});
+        }
+        while(!pq.empty()) {
+            ListNode *topNode = pq.top().second;
+            
+            cur->next = topNode;
+            cur = cur->next;
+            
+            pq.pop();
+            if(topNode->next != nullptr) {
+                pq.push({-topNode->next->val, topNode->next});
             }
             
-            if(minValId == -1) break;
-            cur->next = lists[minValId];
-            cur = cur->next;
-            lists[minValId] = lists[minValId]->next;
         }
+        
         return head->next;
     }
 };
